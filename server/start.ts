ac571b69c7499288f61725532271f5062e31bf19
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-07-05 16:06:02 +0300

    add example colyseus server (blocked on either gae websocket or colyseus tcp transport)

--- server/start.ts
+++ server/start.ts
@@ -1,15 +1,53 @@
+import path from 'path';
 import express from 'express';
-import {X} from '../shared/test';
+import serveIndex from 'serve-index';
+import WebSocket from 'uws';
+import { createServer } from 'http';
+import { Server } from 'colyseus';
+import { monitor } from '@colyseus/monitor';
 
+// Import demo room handlers
+import { ChatRoom } from "./rooms/01-chat-room";
+import { StateHandlerRoom } from "./rooms/02-state-handler";
+import { AuthRoom } from "./rooms/03-auth";
+import { CreateOrJoinRoom } from "./rooms/04-create-or-join-room";
+
+const port = Number(process.env.PORT || 2567);
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send(`Hello, world! got ${X} from shared lib`).end();
+// Attach WebSocket Server on HTTP Server.
+const gameServer = new Server({
+    engine: WebSocket.Server,
+    server: createServer(app)
 });
 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit.');
+// Register ChatRoom as "chat"
+gameServer.register("chat", ChatRoom);
+
+// Register ChatRoom with initial options, as "chat_with_options"
+// onInit(options) will receive client join options + options registered here.
+gameServer.register("chat_with_options", ChatRoom, {
+    custom_options: "you can use me on Room#onInit"
 });
+
+// Register StateHandlerRoom as "state_handler"
+gameServer.register("state_handler", StateHandlerRoom);
+
+// Register StateHandlerRoom as "state_handler"
+gameServer.register("auth", AuthRoom);
+
+// Register CreateOrJoin as "create_or_join"
+gameServer.register("create_or_join", CreateOrJoinRoom);
+
+app.use('/', express.static(path.join(__dirname, "static")));
+app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
+
+// (optional) attach web monitoring panel
+app.use('/colyseus', monitor(gameServer));
+
+gameServer.onShutdown(function(){
+    console.log(`game server is going down.`);
+});
+
+gameServer.listen(port);
+console.log(`Listening on http://localhost:${ port }`);
