3334609503e62a3b30834aa8cf413264550d74b8
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-07-10 03:26:43 +0300

    Updates

--- dist/server2/start.js
+++ dist/server2/start.js
@@ -6,11 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("boardgame.io/server");
 const koa_helmet_1 = __importDefault(require("koa-helmet"));
 const tic_tac_toe_1 = require("../shared/games/tic-tac-toe");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 const server = server_1.Server({ games: [tic_tac_toe_1.TicTacToe] });
 if (process.env.NODE_ENV !== 'development') {
-    server.app.use(koa_helmet_1.default());
+    server.api.use(koa_helmet_1.default());
 }
-server.run(PORT, () => {
-    console.log(`Serving at: http://localhost:${PORT}/`);
-});
+(async () => {
+    await server.db.connect();
+    server.api.listen(PORT, () => {
+        console.log(`Serving at: http://localhost:${PORT}/`);
+    });
+})();
