d32f81ea1cc9a9b5511860ea3ad10bb4e70c4a28
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-07-10 03:59:57 +0300

    re-add app server and mount api server

--- server2/start.ts
+++ server2/start.ts
@@ -1,18 +1,21 @@
 import { Server } from 'boardgame.io/server';
 import KoaHelmet from 'koa-helmet';
+import mount from 'koa-mount';
 import { TicTacToe } from '../shared/games/tic-tac-toe';
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8000;
 
 const server = Server({games: [TicTacToe]});
 
 if (process.env.NODE_ENV !== 'development') {
-    server.api.use(KoaHelmet());
+    server.app.use(KoaHelmet());
 }
+
+server.app.use(mount('/api/v1', server.api));
 
 (async () => {
     await server.db.connect();
-    server.api.listen(PORT, () => {
+    server.app.listen(PORT, () => {
         console.log(`Serving at: http://localhost:${PORT}/`);
     });
 })();
