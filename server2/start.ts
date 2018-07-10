e83bef215cdf7e66208a7e486538a304bfa8d4d9
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-07-10 03:17:29 +0300

    make sure helmet is applied to the correct API

--- server2/start.ts
+++ server2/start.ts
@@ -2,14 +2,17 @@ import { Server } from 'boardgame.io/server';
 import KoaHelmet from 'koa-helmet';
 import { TicTacToe } from '../shared/games/tic-tac-toe';
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 
 const server = Server({games: [TicTacToe]});
 
 if (process.env.NODE_ENV !== 'development') {
-    server.app.use(KoaHelmet());
+    server.api.use(KoaHelmet());
 }
 
-server.run(PORT, () => {
-    console.log(`Serving at: http://localhost:${PORT}/`);
-});
+(async () => {
+    await server.db.connect();
+    server.api.listen(PORT, () => {
+        console.log(`Serving at: http://localhost:${PORT}/`);
+    });
+})();
